74a1e276f7c87c78e206aa5f771e58e39385d5b1
Anuska Gurung <annggrg1@gmail.com>
2024-09-23 16:02:55 +0545

    les go

---
+++ lib/view/onBoarding/components/first_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:music/view/onBoarding/components/boarding_page.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const BoardingPage(
+        text: 'Let\'s vibe togather!Create your\nown playlist and enjoy music',
+        title: 'Customize Your Beast');
+  }
+}
---
+++ lib/view/player/components/song_bottom_contianer.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class SongBottomContainer extends StatelessWidget {
+  const SongBottomContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      width: 320,
+      decoration: BoxDecoration(
+          color: Colors.black12, borderRadius: BorderRadius.circular(40)),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        children: [
+          const Text(
+            'UP NEXT',
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12),
+          ),
+          const Spacer(),
+          Container(
+            height: 20,
+            width: 3,
+            decoration: BoxDecoration(
+                color: Colors.black, borderRadius: BorderRadius.circular(20)),
+          ),
+          const Spacer(),
+          const Text(
+            'LYRICS',
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12),
+          ),
+          const Spacer(),
+          Container(
+            height: 20,
+            width: 3,
+            decoration: BoxDecoration(
+                color: Colors.black, borderRadius: BorderRadius.circular(20)),
+          ),
+          const Spacer(),
+          const Text(
+            'RELATED',
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+}
